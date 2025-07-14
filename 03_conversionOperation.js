698245658913f9a11d1fff4b5022cf526cc2f352
satyajit-pradhan522 <satyajitpradhan522@gmail.com>
2025-07-14 23:19:17 +0530

    js

---
+++ 03_conversionOperation.js
@@ -0,0 +1,69 @@
+
+let score = "33abc"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+let score1 = "null"
+
+
+let valueInNumber1 = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+//console.log(score1);
+
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false 
+// "" => false 
+// "satyajit" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+//******************** Operation ********************
+
+
+let value = 3 
+let negValue = -value
+console.log(negValue);
+
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/2);
+//console.log(2%2);
+
+let str1 = "hello"
+let str2 = " satyajit"
+
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 + 2 + "2");
+console.log("1" + 2 + 2);
+
+
