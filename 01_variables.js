32f41a8400fffbf39059f2fa7e014236d5a74288
satyajit-pradhan522 <satyajitpradhan522@gmail.com>
2025-07-14 13:34:43 +0530

    datatype

---
+++ 01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 122553
+let accountEmail = "satyajitpradhan522@gmail.com"
+var accountPassword = "1234"
+accountCity = "jaipur"
+
+// accountId = 23 
+console.log(accountId);
+
+accountEmail = "ndjihfi@hc.com"
+accountPassword = "212121"
+accountCity = "bengaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 02_dataType.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3)
+
+console.log(3
+    +
+    3) // code readability should be high
+    
+console.log("satyajit")
+
+let name = "satyajit"
+let age = 22
+let isLoggedin = false
+
+// number => 2 to power 53
+// bigint 
+// string => "" 
+// boolean => true/false
+// null => standalone value
+// undefind => 
+// symbol => unique
+// object 
+
+console.log(typeof "satyajit");
+
+console.log(typeof age);
+
+console.log(typeof null);
+console.log(typeof undefined);
